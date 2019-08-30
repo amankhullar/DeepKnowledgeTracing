1ad0909e8dd20dc1830ed858d33f04b24dcb77c9
Aman Khullar <amankhullar97@gmail.com>
2019-08-31 01:36:59 +0530

    minor bugs recolved get train data

--- datasets.py
+++ datasets.py
@@ -20,12 +20,12 @@ class SkillDataset(Dataset):
                 self.students_data = []
                 curr_q = curr_skill = curr_res = ''
                 for row in rows:
-                    if idx % 3:
+                    if idx % 3 == 0:
                         curr_q = row
                         self.max_q = max(self.max_q, int(row[0]))
                     elif idx % 3 == 1:
                         curr_skill = row
-                        self.max_skill = max(self.max_skill, map(int, row))
+                        self.max_skill = max(self.max_skill, max(map(int, row)))
                     else:
                         curr_res = row
                         self.students_data.append((curr_q, curr_skill, curr_res))
@@ -34,14 +34,15 @@ class SkillDataset(Dataset):
             print("Could not find dataset file : " + str(e))
             sys.exit()
         else:
-            assert idx % 3, "Incomplete dataset"
+            assert idx % 3 == 0, "Incomplete dataset"
 
     def __len__(self):
         return len(self.students_data)
 
     def __getitem__(self, idx):
+        print(self.students_data[idx][0])
         student_ques = torch.zeros(self.max_q, 2*(self.max_skill+1))                  # (Padded tensor : (max_ques,2 * (max_skills+1)) of 0s)
-        for ques in range(int(self.students_data[idx][0])):
+        for ques in range(int(self.students_data[idx][0][0])):
             student_ques[ques][int(self.students_data[idx][1][ques])] = 1         # (one-hot encoding of skills)
             if int(self.students_data[idx][-1][ques]) == 1:
                 student_ques[ques][int(self.students_data[idx][1][ques]) + self.max_skill] = 1          # (correct answer)
--- train.py
+++ train.py
@@ -8,17 +8,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from torch.utils.data import data
-
 from datasets import SkillDataset
 
 def main(train_file_path, batch_size):
     skill_dataset = SkillDataset(train_file_path)
     train_skill_loader = torch.utils.data.DataLoader(skill_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
     
-    
+    for (student, student_orig) in train_skill_loader:
+        print("Student size is : {}".format(student.size()))
+        print("Student padded value is  : {}".format(student))
+        print("Original student values are : {}".format(student_orig))
+        print("Original student type is : {}".format(type(student_orig)))
+        break
 
 if __name__ == "__main__":
-    train_file_path = ""
-    batch_size = 1
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    train_file_path = os.path.join(base_dir, 'data', '0910_a_test.csv')
+    batch_size = 3
     main(train_file_path, batch_size)
